3c6fbc25b3f70883ddc2dc798a2cfa5343e76e67
RobertHechler <141368329+RobertHechler@users.noreply.github.com>
2025-01-18 21:28:53 +0000

    Deploying to gh-pages from @ RobertHechler/RobertHechler.github.io@1e2125ca786b4ddc1263f0b34c58453b1a776457 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,16 +52,16 @@ ninja.data = [{
           window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-received-the-2024-harry-smith-prize-for-the-best-paper-published-by-an-ecr-in-molecular-ecology-links-to-the-paper-editorial-and-announcement",
+          title: 'Received the 2024 Harry Smith Prize for the best paper published by an...',
           description: "",
           section: "News",},{
         id: 'social-email',
